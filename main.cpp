a4cfbbe4dd548b146fb63553a310291bd34b41a0
jakob-a2k <jakub.a2k@gmail.com>
2022-09-24 20:38:13 +0200

    My own version

--- main.cpp
+++ main.cpp
@@ -3,200 +3,122 @@
 
 using namespace std;
 
-int player;
-char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-char who_play;              // gra kolko czy krzyzyk
 
-void game();
-void draw_board();
-void pick_slot(int slot);
-void check();               //sprawdzmy czy ktos wygral
+int first_or_second;
+char o_or_x;
+char tab[9] = {'1','2','3','4','5','6','7','8','9'};
+
+void draw_a_board();
+void set_to_board(int choosen_number);
 void change_player();
+void test_if_win();
+
 
 int main(){
 
+    cout << " This is our game table\n";
+    draw_a_board();
+    cout << "\n Choose your character( 'x' or 'o' ): ";
+    char sign;
+    cin >> sign;
+    o_or_x = sign;
 
-    game();
+    first_or_second = 1;
 
+    for( char i = 1; i < 9; i++ ){
+        cout << " \nPlayer " << first_or_second << " now it is your turn !\n";
+        cout << " Insert " << o_or_x << " to number: ";
+        int choosen_number;
+        cin >> choosen_number;
 
+        set_to_board( choosen_number );
+
+        draw_a_board();
+
+        change_player();
+
+        test_if_win();
+    }
+    cout << "To byla zacieta rozgrywka! Probojcie dalej!\n";
     return 0;
 }
 
 
-void game(){
-    cout << " Plyer one, choose your marker: ";
-    char marker_1;
-    cin >> marker_1;
+void draw_a_board(){
 
-    player = 1;
-    who_play = marker_1;
-
-    draw_board();
-
-    // mozna wykonac maksymalnie 9 ruchow poniewaz tyle jest pól
     for(int i = 0; i < 9; i++){
-        cout << "It is player " << player << " turn. Enter a slot: ";
-        int slot;
-        cin >> slot;
-
-        pick_slot(slot);
-
-        draw_board();
-
-        change_player();
+        if( i < 2 || i > 2 && i < 5 || i > 5 && i < 8){
+            cout << tab[i] << " | ";
+        }
+        else if( i % 2 == 0 ){
+            cout << tab[i];
+            if(i != 8){
+                cout << "\n---------\n";
+            }
+        }
+        else if( i % 5 == 0 ){
+            cout << tab[i];
+            if(i != 8){
+                cout << "\n---------\n";
+            }
+        }
     }
-    cout << " Tic_Tac_Toe ended in a draw!\n";
-
-}
-
-
-void pick_slot( int slot){
-
-    if(slot == 1){
-        if(who_play == 'o'){
-            board[0] = 'o';
-            who_play = 'x';
-
-        }
-        else{
-            board[0] = 'x';
-            who_play = 'o';
-        }
-        check();
-    }
-
-    if(slot == 2){
-        if(who_play == 'o'){
-            board[1] = 'o';
-            who_play = 'x';
-        }
-        else{
-            board[1] = 'x';
-            who_play = 'o';
-        }
-        check();
-    }
-
-    if(slot == 3){
-        if(who_play == 'o'){
-            board[2] = 'o';
-            who_play = 'x';
-        }
-        else{
-            board[2] = 'x';
-            who_play = 'o';
-        }
-        check();
-    }
-
-    if(slot == 4){
-        if(who_play == 'o'){
-            board[3] = 'o';
-            who_play = 'x';
-        }
-        else{
-            board[3] = 'x';
-            who_play = 'o';
-        }
-        check();
-    }
-
-    if(slot == 5){
-        if(who_play == 'o'){
-            board[4] = 'o';
-            who_play = 'x';
-        }
-        else{
-            board[4] = 'x';
-            who_play = 'o';
-        }
-        check();
-    }
-
-    if(slot == 6){
-        if(who_play == 'o'){
-            board[5] = 'o';
-            who_play = 'x';
-        }
-        else{
-            board[5] = 'x';
-            who_play = 'o';
-        }
-        check();
-    }
-
-    if(slot == 7){
-        if(who_play == 'o'){
-            board[6] = 'o';
-            who_play = 'x';
-        }
-        else{
-            board[6] = 'x';
-            who_play = 'o';
-        }
-        check();
-    }
-
-    if(slot == 8){
-        if(who_play == 'o'){
-            board[7] = 'o';
-            who_play = 'x';
-        }
-        else{
-            board[7] = 'x';
-            who_play = 'o';
-        }
-        check();
-    }
-
-    if(slot == 9){
-        if(who_play == 'o'){
-            board[8] = 'o';
-            who_play = 'x';
-        }
-        else{
-            board[8] = 'x';
-            who_play = 'o';
-        }
-        check();
-    }
-}
-
-
-void check  (){
-     if((board[0] == board[1] && board[1] == board[2] && board[0] != '1') ||       // sprawdzenie kto wygral
-        (board[3] == board[4] && board[4] == board[5] && board[0] != '1') ||       // w pionie, poziomie, skos
-        (board[6] == board[7] && board[7] == board[8] && board[0] != '1') ||
-        (board[0] == board[3] && board[3] == board[6] && board[0] != '1') ||
-        (board[1] == board[4] && board[4] == board[7] && board[0] != '1') ||
-        (board[2] == board[5] && board[5] == board[8] && board[0] != '1') ||
-        (board[0] == board[4] && board[8] == board[2] && board[0] != '1') ||
-        (board[2] == board[4] && board[4] == board[6] && board[0] != '1') ){
-
-             draw_board();
-
-             if( who_play == 'x'){
-                cout << " Circle WINS!\n";
-             }
-             else{
-                cout << " Cross WINS!\n";
-             }
-             cout << " Game Over!";
-             exit(0);
-     }
-}
-
-
-void draw_board(){
-    cout << " " << board[0] << " | " << board[1] << " | " << board[2] << endl;     // narysowanie planszy
-    cout << "----------\n";
-    cout << " " << board[3] << " | " << board[4] << " | " << board[5] << endl;
-    cout << "----------\n";
-    cout << " " << board[6] << " | " << board[7] << " | " << board[8] << endl;
 }
 
 
 void change_player(){
-    if( player == 1 ) player = 2;
-    else player = 1;
+    if( o_or_x == 'x') o_or_x ='o';
+    else o_or_x = 'x';
+
+    if( first_or_second == 1 ) first_or_second = 2;
+    else first_or_second = 1;
 }
+
+
+void set_to_board( int choosen_number ){
+    if( choosen_number == 1 ){
+        tab[0] = o_or_x;
+    }
+    else if( choosen_number == 2){
+        tab[1] = o_or_x;
+    }
+    else if( choosen_number == 3){
+        tab[2] = o_or_x;
+    }
+    else if( choosen_number == 4){
+        tab[3] = o_or_x;
+    }
+    else if( choosen_number == 5){
+        tab[4] = o_or_x;
+    }
+    else if( choosen_number == 6){
+        tab[5] = o_or_x;
+    }
+    else if( choosen_number == 7){
+        tab[6] = o_or_x;
+    }
+    else if( choosen_number == 8){
+        tab[7] = o_or_x;
+    }
+    else if( choosen_number == 9){
+        tab[8] = o_or_x;
+    }
+}
+
+
+void test_if_win(){
+    if( tab[0] == tab[1] && tab[1] == tab[2] ||
+        tab[3] == tab[4] && tab[4] == tab[5] ||
+        tab[6] == tab[7] && tab[7] == tab[8] ||
+        tab[0] == tab[3] && tab[3] == tab[6] ||
+        tab[1] == tab[4] && tab[4] == tab[7] ||
+        tab[2] == tab[5] && tab[5] == tab[8] ||
+        tab[0] == tab[4] && tab[4] == tab[8] ||
+        tab[2] == tab[4] && tab[4] == tab[6] ){
+            if( o_or_x == 'x') { cout << " \nWygywa kolko\n"; }
+            else    cout << " \nWygrywa krzyzyk\n";
+            cout << " Koniec gry!\n"; exit(0);
+        }
+}
+
+
