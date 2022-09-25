b113a9e329ce7602c67012606483e653b2b38588
jakob-a2k <jakub.a2k@gmail.com>
2022-09-25 10:54:14 +0200

    code refactoring

--- main.cpp
+++ main.cpp
@@ -3,75 +3,71 @@
 
 using namespace std;
 
-
-int first_or_second;
 char o_or_x;
-char tab[9] = {'1','2','3','4','5','6','7','8','9'};
+char tab[9]; //= {'1','2','3','4','5','6','7','8','9'};
 
+void show_available_options();
 void draw_a_board();
 void set_to_board(int choosen_number);
 void change_player();
 void test_if_win();
 
 
+
 int main(){
 
-    cout << " This is our game table\n";
-    draw_a_board();
-    cout << "\n Choose your character( 'x' or 'o' ): ";
-    char sign;
-    cin >> sign;
-    o_or_x = sign;
+    o_or_x = 'o';
 
-    first_or_second = 1;
+    for( char i = 1; i < 10; i++ ){
+        cout << endl;
+        show_available_options();
+        cout << endl;
+        draw_a_board();
 
-    for( char i = 1; i < 9; i++ ){
-        cout << " \nPlayer " << first_or_second << " now it is your turn !\n";
-        cout << " Insert " << o_or_x << " to number: ";
+        cout << "\nWybierz od 1 do 9\n";
         int choosen_number;
         cin >> choosen_number;
 
         set_to_board( choosen_number );
 
-        draw_a_board();
-
         change_player();
 
         test_if_win();
     }
-    cout << "To byla zacieta rozgrywka! Probojcie dalej!\n";
+    cout << "Remis\n";
+
     return 0;
+}
+
+
+void show_available_options(){
+    for(int i = 1; i < 10; i++){
+        if(i < 3 || i > 3 && i < 6 || i > 6 && i < 9){
+            cout << i << " | ";
+        }
+        else if(i % 3 == 0){
+            cout << i << "\n";
+            if(i != 9){
+                cout << "---------\n";
+            }
+        }
+    }
 }
 
 
 void draw_a_board(){
 
-    for(int i = 0; i < 9; i++){
-        if( i < 2 || i > 2 && i < 5 || i > 5 && i < 8){
-            cout << tab[i] << " | ";
-        }
-        else if( i % 2 == 0 ){
-            cout << tab[i];
-            if(i != 8){
-                cout << "\n---------\n";
-            }
-        }
-        else if( i % 5 == 0 ){
-            cout << tab[i];
-            if(i != 8){
-                cout << "\n---------\n";
-            }
-        }
-    }
+    cout << tab[0] << " | " << tab[1] << " | " << tab[2] << "\n";
+    cout << "---------\n";
+    cout << tab[3] << " | " << tab[4] << " | " << tab[5] << "\n";
+    cout << "---------\n";
+    cout << tab[6] << " | " << tab[7] << " | " << tab[8] << "\n";
 }
 
 
 void change_player(){
     if( o_or_x == 'x') o_or_x ='o';
     else o_or_x = 'x';
-
-    if( first_or_second == 1 ) first_or_second = 2;
-    else first_or_second = 1;
 }
 
 
@@ -107,15 +103,17 @@ void set_to_board( int choosen_number ){
 
 
 void test_if_win(){
-    if( tab[0] == tab[1] && tab[1] == tab[2] ||
-        tab[3] == tab[4] && tab[4] == tab[5] ||
-        tab[6] == tab[7] && tab[7] == tab[8] ||
-        tab[0] == tab[3] && tab[3] == tab[6] ||
-        tab[1] == tab[4] && tab[4] == tab[7] ||
-        tab[2] == tab[5] && tab[5] == tab[8] ||
-        tab[0] == tab[4] && tab[4] == tab[8] ||
-        tab[2] == tab[4] && tab[4] == tab[6] ){
-            if( o_or_x == 'x') { cout << " \nWygywa kolko\n"; }
+    if( tab[0] == tab[1] && tab[1] == tab[2] && tab[0] == 'o' || tab[0] == tab[1] && tab[1] == tab[2] && tab[0] == 'x' ||
+        tab[3] == tab[4] && tab[4] == tab[5] && tab[3] == 'o' || tab[3] == tab[4] && tab[4] == tab[5] && tab[3] == 'x' ||
+        tab[6] == tab[7] && tab[7] == tab[8] && tab[6] == 'o' || tab[6] == tab[7] && tab[7] == tab[8] && tab[6] == 'x' ||
+        tab[0] == tab[3] && tab[3] == tab[6] && tab[0] == 'o' || tab[0] == tab[3] && tab[3] == tab[6] && tab[0] == 'x' ||
+        tab[1] == tab[4] && tab[4] == tab[7] && tab[1] == 'o' || tab[1] == tab[4] && tab[4] == tab[7] && tab[1] == 'x' ||
+        tab[2] == tab[5] && tab[5] == tab[8] && tab[2] == 'o' || tab[2] == tab[5] && tab[5] == tab[8] && tab[2] == 'x' ||
+        tab[0] == tab[4] && tab[4] == tab[8] && tab[0] == 'o' || tab[0] == tab[4] && tab[4] == tab[8] && tab[0] == 'x' ||
+        tab[2] == tab[4] && tab[4] == tab[6] && tab[2] == 'o' || tab[2] == tab[4] && tab[4] == tab[6] && tab[2] == 'x' ){
+
+            draw_a_board();
+            if( o_or_x == 'x') { cout << " \nWygrywa kolko\n"; }
             else    cout << " \nWygrywa krzyzyk\n";
             cout << " Koniec gry!\n"; exit(0);
         }
